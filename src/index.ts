bff7a0eafc15c0bfbac7f59685a1df8d881c5d15
Yakshap <yakshap@czlabs.io>
2025-03-22 14:04:56 +0530

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,14 @@ const wss = new WebSocketServer({ port: 8080 });
 let userCount = 0;
 
 wss.on("connection",(socket)=>{
-    console.log("user connected");
     userCount = userCount + 1;
+
+    console.log("user connected #",userCount);
+
+    socket.on("message",(message)=>{
+        console.log("message received", message.toString())
+        setTimeout(()=>{
+            socket.send(message.toString() + ": sent from the server.")
+        },1000)
+    })
 })
